bba1a219215500197704547be335a19da05486ab
Sean Cleaver <sean@simego.com>
2022-08-03 16:20:28 +0100

    Initial commit

---
+++ src/Simego.DataSync.Providers.MySQL/MySqlDatasourceReader.cs
@@ -0,0 +1,28 @@
+using Simego.DataSync.Providers.Ado;
+using System;
+using System.Data.Common;
+using System.Globalization;
+using System.Reflection;
+using MySql.Data.MySqlClient;
+
+namespace Simego.DataSync.Providers.MySQL
+{
+    [ProviderInfo(Name = "MySQL (8.0.30)", Group = "SQL", Description = "MySql.Data.MySqlClient by Oracle")]
+    public class MySqlDatasourceReader : AdoDataSourceReader
+    {
+        private Lazy<AdoDbProviderFactory> MyFactory => new Lazy<AdoDbProviderFactory>(() => new AdoDbProviderFactory("MySql.Data", GetFactory()));
+        
+        protected override bool IsCustomAdoProvider() => true;
+
+        public override AdoDbProviderFactory GetProviderFactory(string providerInvariantName) => MyFactory.Value;
+
+        private static DbProviderFactory GetFactory()
+        {
+            var factoryType = typeof(MySqlClientFactory); 
+
+            return factoryType.InvokeMember(factoryType.Name,
+               BindingFlags.CreateInstance | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public,
+               null, null, null, CultureInfo.CurrentCulture) as DbProviderFactory;            
+        }
+    }
+}
