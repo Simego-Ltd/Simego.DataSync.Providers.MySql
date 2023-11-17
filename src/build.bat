e7035a00ba8379d3090decc0aaa8b00f17cc03ed
Sean Cleaver <sean@simego.com>
2023-11-17 13:10:45 +0000

    remove 3rd party folder and use nuget package manager

--- src/build.bat
+++ src/build.bat
@@ -2,7 +2,7 @@ set p=Simego.DataSync.Providers.MySQL
 dotnet build -c Release
 rmdir ..\dist\ /S /Q
 mkdir ..\dist\files\%p%
-xcopy ..\src\%p%\bin\Release\net7.0-windows\*.* ..\dist\files\%p%\*.* /y /s
+xcopy ..\src\%p%\bin\Release\net8.0-windows7.0\*.* ..\dist\files\%p%\*.* /y /s
 cd ..\dist\files\
 del .\%p%\Simego.DataSync.Core.dll
 del .\%p%\Simego.DataSync.Providers.Ado.dll
