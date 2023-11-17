fb4abf7c5b50c84bd69df25c17703dfb83fba25f
Sean Cleaver <sean@simego.com>
2023-11-17 13:55:01 +0000

    Update build.bat

--- src/build.bat
+++ src/build.bat
@@ -6,6 +6,8 @@ xcopy ..\src\%p%\bin\Release\net8.0-windows7.0\*.* ..\dist\files\%p%\*.* /y /s
 cd ..\dist\files\
 del .\%p%\Simego.DataSync.Core.dll
 del .\%p%\Simego.DataSync.Providers.Ado.dll
+del .\%p%\Simego.DataSync.Security.dll
+del .\%p%\Simego.DataSync.SqlSchemaNavigator.dll
 tar.exe -acf ..\%p%.zip *.*
 cd ..\..\src
 
